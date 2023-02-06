9eeecfecaf9d6b2f144ef86aa8d7505101c79000
Ernest Haruna <ernestharuna1@gmail.com>
2023-02-06 16:51:54 +0100

    Some changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,21 @@
+import PropTypes from 'prop-types'
+
+// Button component
 const Button = ({ color, text }) => {
-    return <button style={{ backgroundColor: color }} className="btn">{text}</button>
+    const foo = () => {
+        // alert('I was clicked');
+    }
+    return <button onClick={foo} style={{ backgroundColor: color }} className="btn">{text}</button>
+}
+// Component end
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import Button from "./Button"
 
 const Header = (props) => {
+    const foo = () => {
+        alert('Hello')
+    }
+
     return (
         <header className="header">
             <h1>{props.title}</h1>
-            <Button color='green' text='Hello' />
+            <Button color='green' text='Add' foo='foo' />
         </header>
     )
 }
