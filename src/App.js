431ee393f1676f2f8b396f9265afd6e4ccf3f6e2
Ernest Haruna <ernestharuna1@gmail.com>
2023-02-06 14:23:30 +0100

    initial components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
+  const name = "Ernest"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='Plango' />
+      <p>Hello <b>{name}</b></p>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Button from "./Button"
+
+const Header = (props) => {
+    return (
+        <header className="header">
+            <h1>{props.title}</h1>
+            <Button color='green' text='Hello' />
+        </header>
+    )
+}
+
+export default Header
